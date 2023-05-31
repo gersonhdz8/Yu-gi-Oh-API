22020e061e5b660d24f790c6d26b108b7ab4f136
GersonHdz <gersonhdz8@gmail.com>
2023-05-30 19:30:18 -0500

    feat: :sparkles: Cambio rutas tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,8 @@
 module.exports = {
   content: ["./index.html","./public/*.{html,js}"],
   theme: {
-    extend: {      
-      backgroundImage:{
-        "fondoCarta":"url('/images/fondoCarta.jpg')",
-        "fondoCarta2":"url('/images/fondoCarta2.jpg')",
-        "fondoCarta3":"url('/images/fondoCarta3.jpg')",                 
-
-      },
+    extend: {     
+    
     }
   },
   plugins: [],
